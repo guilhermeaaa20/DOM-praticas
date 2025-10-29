8f001cf86016c3ba79b4a602f21604be14828cda
guilhermeaaa20 <guilhermeaaa20@gmail.com>
2025-10-29 10:41:13 -0300

    Update index.html
    Atividade DOM das cores feitas.

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,45 @@ document.getElementById("retangulo").style.backgroundColor = "red";
 document.getElementById("circulo").style.backgroundColor = "purple";
 
 //Exercício 4:
+document.getElementsByClassName("quad");
+let divs = document.getElementsByClassName("quad");
+divs[0].style.backgroundColor = "blue";
+divs[1].style.backgroundColor = "orange";
+divs[2].style.backgroundColor = "pink";
+
+//Exercício 5:
+document.getElementsByClassName("parag");
+document.getElementById("paragcor").style.color = "white";
+let paragrafo = document.getElementsByClassName("parag");
+paragrafo[0].style.backgroundColor = "black";
+
+//Exercício 6:
+document.getElementById("banner").style.backgroundColor = "#333333"
+
+//Exercício 7:
+let mudarcordobottom = document.getElementById("triangulo");
+mudarcordobottom.style.borderBottom = "100px solid #32CD32";
+
+//Exercício 8:
+document.getElementById("circ").style.backgroundColor = "blue";
+document.getElementById("quadra").style.backgroundColor = "red";
+
+//Exercícios 9:
+document.getElementById("titulotop").style.background = "yellow";
+document.getElementById("titulotop").style.color = "green";
+
+//Exercícios 10:
+document.getElementsByClassName("coresgerais");
+let divisoes = document.getElementsByClassName("coresgerais");
+divisoes[0].style.background = "red";
+divisoes[1].style.background = "orange";
+divisoes[2].style.background = "yellow";
+divisoes[3].style.background = "green";
+divisoes[4].style.background = "blue";
+
+//Exercícios 11:
+document.getElementById("rosapreto").style.backgroundColor = "black";
+document.getElementById("rosapreto").style.color = "pink";
+
+//Exercícios 12:
+document.getElementById("elipse").style.background = "#00b3a7ff";
