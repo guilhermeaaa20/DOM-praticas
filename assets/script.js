830e1a917be38c469c89f9f71392dbe3e80f5d0e
guilhermeaaa20 <guilhermeaaa20@gmail.com>
2025-10-29 02:11:08 -0300

    Update index.html
    comecei uma nova atividade de Js sobre cores

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//Exercícios para praticar mudança automática de cores com JavaScript
+//Exercício 1...
+
+//Exercício 2:
+document.getElementById("retangulo").style.backgroundColor = "red";
+
+//Exercício 3:
+document.getElementById("circulo").style.backgroundColor = "purple";
+
+//Exercício 4:
